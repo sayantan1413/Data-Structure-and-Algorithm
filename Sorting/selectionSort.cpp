5fc95ee26d607c18b1be8a94d5d34fb7d7e3997e
sayantan1413 <sayantanbose2001@gmail.com>
2021-04-07 20:33:35 +0530

    Added Selection Sort in C++

---
+++ Sorting/selectionSort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int32_t main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j] < arr[i])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
