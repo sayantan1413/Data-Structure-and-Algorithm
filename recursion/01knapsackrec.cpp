dbc0f997e90f270ac0961d7b26caad52ef9ca433
Sayantan Bose <64970095+sayantan1413@users.noreply.github.com>
2021-05-21 23:33:29 +0530

    Add files via upload

---
+++ recursion/01knapsackrec.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int knapsack(int val[], int wt[], int n, int weight)
+{
+    if (n == 0 || weight == 0)
+        return 0;
+    if (wt[n - 1] <= weight)
+        return max((val[n - 1] + knapsack(val, wt, n - 1, weight - wt[n - 1])), knapsack(val, wt, n - 1, weight));
+    else
+        return knapsack(val, wt, n - 1, weight);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int val[n];
+    int wt[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> val[i];
+    }
+    for (int j = 0; j < n; j++)
+    {
+        cin >> wt[j];
+    }
+    int weight;
+    cin >> weight;
+    cout << knapsack(val, wt, n, weight) << endl;
+    return 0;
+}
